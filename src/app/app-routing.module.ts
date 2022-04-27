d0ca301a1e8cc6c2d6bbd12bf7824fdd08ecb713
ZathSL <93914398+ZathSL@users.noreply.github.com>
2022-04-27 15:21:27 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {ProfileComponent} from "./profile/profile.component";
-import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: LoginComponent},
   { path:'registration', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
   { path: 'users', component: ProfileComponent}

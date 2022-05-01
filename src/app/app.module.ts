2640b36fcb139b04223038779031e7ccb71b09f0
Alessio <51161852+alessiofamiani@users.noreply.github.com>
2022-05-01 11:24:15 +0200

    + Login design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {GoogleLoginProvider,
         SocialAuthServiceConfig} from 'angularx-social-login'
 import {CoolSocialLoginButtonsModule} from '@angular-cool/social-login-buttons';
 import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -22,7 +22,7 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     RegisterComponent,
     ProfileComponent,
-    HomeComponent,
+    //HomeComponent,
   ],
   imports: [
     BrowserModule,

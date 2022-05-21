1a96002a740fb87e32e13840fb367d1ba24bd6e0
ZathSL <simone.lissandrello@edu.unito.it>
2022-05-21 16:56:38 +0200

    some improvements

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,7 @@ declare var process: {
   env: {
     NG_APP_ENV: string;
     NG_APP_GATEWAY_URL: string;
-    NG_AUTH_TOKEN: string;
+    NG_APP_AUTH_TOKEN: string;
     // Replace the line below with your environment variable for better type checking
     [key: string]: any;
   };
